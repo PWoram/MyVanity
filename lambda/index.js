b26c08a0d12d50c6ab366d3c4550c414bf5a9947
Philip Woram <git-commit-address>
2021-03-03 12:39:40 -0600

    Updated npm packages. Implemented for loop to ensure results array is not empty before storing data in dynamoDB table.

--- lambda/index.js
+++ lambda/index.js
@@ -1,81 +1,87 @@
 const AWS = require('aws-sdk');
-// const dynamodb = new.AWS.DynamoDB();
+const dynamodb = new AWS.DynamoDB({
+	region: 'us-east-1',
+	apiVersion: '2012-08-10',
+});
 var checkWord = require('check-word'),
 	words = checkWord('en');
 
+const phoneDigitsToLetters = {
+	0: '0',
+	1: '1',
+	2: 'abc',
+	3: 'def',
+	4: 'ghi',
+	5: 'jkl',
+	6: 'mno',
+	7: 'pqrs',
+	8: 'tuv',
+	9: 'wxyz',
+};
+
+//helper function to return substrings from a phone number
+const numberSubStrings = function (phoneNumber) {
+	var subStrings = [];
+
+	for (var i = 0; i < phoneNumber.length; i++) {
+		subStrings.push(phoneNumber.slice(i));
+	}
+	return subStrings;
+};
+
+//primary function to convert a number string into words
+const generateWords = function (numberString) {
+	if (!numberString.length) {
+		return [''];
+	}
+
+	var digitsArray = numberString.split('');
+	var results = [];
+
+	//inner recursive function to generate letter combinations from phone number digits
+	var lettersFromDigits = function (word, digitsArray) {
+		//base case: if no digits remain, check whether a word is formed and if so push to results array
+		if (!digitsArray.length) {
+			if (words.check(word)) {
+				results.push(word);
+			}
+			return;
+		}
+		//recursive case: take current digit from digitsArray
+		var letters = phoneDigitsToLetters[digitsArray[0]];
+
+		for (var i = 0; i < letters.length; i++) {
+			lettersFromDigits(word + letters[i], digitsArray.slice(1));
+		}
+	};
+	lettersFromDigits('', digitsArray);
+
+	return results;
+};
+
 //handler method to store data in dynamoDB table
 module.exports.handler = (event, context, callback) => {
-	const phoneDigitsToLetters = {
-		0: '0',
-		1: '1',
-		2: 'abc',
-		3: 'def',
-		4: 'ghi',
-		5: 'jkl',
-		6: 'mno',
-		7: 'pqrs',
-		8: 'tuv',
-		9: 'wxyz',
-	};
-
-	const numberSubStrings = function (phoneNumber) {
-		var subStrings = [];
-
-		for (var i = 0; i < phoneNumber.length; i++) {
-			subStrings.push(phoneNumber.slice(i));
-			// subStrings.push(phoneNumber.slice(-i));
-		}
-
-		return subStrings;
-	};
-
-	const generateWords = function (numberString) {
-		if (!numberString.length) {
-			return [''];
-		}
-
-		var digitsArray = numberString.split('');
-
-		// if (digitsArray.includes(0) || digitsArray.includes(1)) {
-		// 	return 'No vanity numbers available!';
-		// }
-
-		var results = [];
-
-		//recursive function to generate letter combinations from phone number digits
-		var lettersFromDigits = function (word, digitsArray) {
-			//base case: if no digits remain, check whether a word is formed and if so push to results array
-			if (!digitsArray.length) {
-				if (words.check(word)) {
-					results.push(word);
-				}
-				return;
-			}
-			//recursive case: take current digit from digitsArray
-			var letters = phoneDigitsToLetters[digitsArray[0]];
-
-			for (var i = 0; i < letters.length; i++) {
-				lettersFromDigits(word + letters[i], digitsArray.slice(1));
-			}
-		};
-		lettersFromDigits('', digitsArray);
-
-		return results;
-	};
-
-	var number = event.Details.ContactData.CustomerEndpoint.Address;
-	// var testNumber = '7962229';
-	var subStrings = numberSubStrings(number);
+	var inputNumber = event.Details.ContactData.CustomerEndpoint.Address.slice(8);
+	var subStrings = numberSubStrings(inputNumber);
 	subStrings = subStrings.slice(0, 4);
 	var vanityNumbers = [];
-	subStrings.forEach((i) => vanityNumbers.push(generateWords(i)));
+	subStrings.forEach((subString) =>
+		vanityNumbers.push(generateWords(subString))
+	);
 	var results = vanityNumbers.flat();
-	// console.log('results:', results);
+	//loop through results array to make sure the first five indicies are defined
+	for (var i = 0; i < 5; i++) {
+		if (results[i]) {
+			continue;
+		} else {
+			results[i] = 'none';
+		}
+	}
 
 	const params = {
 		Item: {
 			phoneNumber: {
-				S: number,
+				S: event.Details.ContactData.CustomerEndpoint.Address,
 			},
 			vanityName: {
 				S: results[0],
@@ -97,7 +103,7 @@ module.exports.handler = (event, context, callback) => {
 	};
 	dynamodb.putItem(params, function (err, data) {
 		if (err) {
-			console.log(err);
+			console.log('there was an error:', err);
 			callback(err);
 		} else {
 			console.log(data);
